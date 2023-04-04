16712fe7977f1fc51c90601034b688442efcc701
Joel <jpeddhi@contractor.victoria.com>
2023-04-04 11:52:27 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by JOEL PEDDHI on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
