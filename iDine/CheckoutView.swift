8d5523ec331201fb2d8ee4c1b59407101653129f
Joel <jpeddhi@contractor.victoria.com>
2023-04-04 19:07:26 +0530

    First commit

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by JOEL PEDDHI on 04/04/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView()
+    }
+}
